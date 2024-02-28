3f0a900ea0a473fe8d9eeeb1c587e50bebca574e
voidreamy <2707786374wsj@gmail.com>
2024-02-28 22:13:22 +0800

    first version

---
+++ Hello.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+st.write("# Welcome to Birthday Party Plan! 👋")
+st.sidebar.success("Hello.")
+st.markdown("""### Hello""")
+
